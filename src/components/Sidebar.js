0a8f147feb1723bb6c953e936a171499fd9cfc17
Monsoir <monwingyeung@gmail.com>
2017-12-10 19:17:40 +0800

    What: 1.整体布局

---
+++ src/components/Sidebar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import './sidebar.css';
+
+export default class Sidebar extends Component {
+  render() {
+    return (
+      <div className="sidebar" />
+    );
+  }
+}
